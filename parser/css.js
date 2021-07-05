996d8e4ff43af2dbde1ad7bf78256285404e45b5
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-07-05 14:15:05 +0300

    this should work... right?

--- parser/css.js
+++ parser/css.js
@@ -1,3 +1,4 @@
+const { removeMultilineComments } = require("./util.js");
 const warn = console.warn;
 
 /*
@@ -32,8 +33,51 @@ function parseBlock(lines){
 	return block;
 };
 
+function parseSelections(content){
+	return [content]; // TODO
+};
+
+/**
+* @typedef {object[]} CSSData
+* @prop {string[]} selection - ex: "h1"
+* @prop {object} block - ex: { color: "red" }
+*/
+
+/**
+* Parse CSS
+* @param {string} content
+* @returns {CSSData}
+*/
 function parse(content){
-	
+	let data = [];
+	content = removeMultilineComments(content);
+	content = content.replace(/\n/g, ""); // ::Would this break it?
+	let current = "selections"; // ('selections'|'block')
+	let selection = null;
+	let buf = "";
+	for(let char of content) {
+		switch(char){
+			case "{":
+				// Parse the selections and begin saving the block
+				selection = parseSelections(buf.trim());
+				buf = "";
+				current = "block";
+				break;
+			case "}":
+				// End the block and parse it
+				data.push({
+					selection,
+					block: parseBlock(buf.trim()),
+				});
+				selection = null;
+				current = "selections";
+				buf = "";
+				break;
+			default:
+				buf += char;
+		};
+	};
+	return data;
 };
 
 module.exports = { parse, parseBlock, parseLine };
