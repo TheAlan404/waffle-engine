15ce120d504f5d7f3b082e89b61f61e7ec857eb7
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-07-08 10:18:04 +0300

    Update util.js

--- src/parser/util.js
+++ src/parser/util.js
@@ -1,9 +1,15 @@
 const multilineCommentRegex = /\/\*[\s\S]*\*\//g;
+const htmlCommentRegex = /<!--[\s\S]*-->/g,
 
 function removeMultilineComments(text){
 	return text.replace(multilineCommentRegex, "");
 };
 
+function removeHTMLComments(text){
+	return text.replace(htmlCommentRegex, "");
+};
+
 module.exports = {
 	removeMultilineComments,
+	removeHTMLComments,
 };
