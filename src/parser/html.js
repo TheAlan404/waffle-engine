fc76bfef91126b07fc3169cd9e106c1dffcd95e9
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-07-08 10:39:57 +0300

    Update html.js

--- src/parser/html.js
+++ src/parser/html.js
@@ -1,9 +1,27 @@
-const { parse: parseXML } = require("xml-parser");
-
+const { removeHTMLComments } = require("./util.js");
+const { HTMLElement, TextNode } = require("./html/Element.js");
 
 function parse(content){
-	return parseXML(content);
+	content = removeHTMLComments(content);
+	let state = "text"; // (text|tagblank|tagname|tagattrname|tagattrvalue)
+	let root = new HTMLElement();
+	let current = root;
+	let buf = "";
+	for(let char of content){
+		switch(char){
+			case "<":
+				if(state !== "text") throw new Error("HTMLParser: Illegal State on '<' eccounter");
+				if(buf.trim().length) current.children.push(new TextNode(buf.trim()));
+				buf = "";
+				state = "tagname";
+				break;
+			case " ":
+				if(state === "tagname") {
+					if(!buf.length) break; // EX: < body >hi</body >
+				};
+		};
+	};
 };
-
+// i gibe up
 
 //
